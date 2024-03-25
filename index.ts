6039a85ae11ce390f74c3e7c6b3307c0ae1f99a5
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-03-25 16:55:44 +0500

    First commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+// Shabang
+
+import inquirer from "inquirer";
+
+// Computer will generate a random number- Done
+
+// User input for guessing number-  Done
+
+// Compare user input with computer generated number- Done
+
+const randomNumber= Math.floor(Math.random()*6+1);
+
+const answer= await inquirer.prompt([
+ {message:"Please guess a number between 1-6: ", type:"number", name: "userGuessedNumber"}   
+]);
+
+// Conditional statement
+
+if(answer.userGuessedNumber===randomNumber){
+    console.log("Congratulations! your gussed the right number");
+} else{
+    console.log("Sorry you gussed the wrong number");
+}
+
+
